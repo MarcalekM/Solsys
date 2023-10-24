f1f55fa684e413e664389dab773c41d1e4eac59f
Máté Marcalek <matemarcalek@gmail.com>
2023-10-24 11:36:47 +0200

    Add project files.

---
+++ Solsys/Planet.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solsys
+{
+    internal class Planet
+    {
+        public string Nev { get; set; }
+        public int Holdak { get; set; }
+        public double   Terfogat { get; set; }
+
+        public  Planet(string s)
+        {
+            var v = s.Split(';');
+            Nev = v[0];
+            Holdak = int.Parse(v[1]);
+            Terfogat = double.Parse(v[2]);
+        }
+    }
+}
---
+++ Solsys/Program.cs
@@ -0,0 +1,19 @@
+﻿using Solsys;
+
+List<Planet> planets = new();
+using StreamReader sr = new(
+    path: @"..\..\..\src.solsys.txt",
+    encoding: System.Text.Encoding.UTF8);
+while (!sr.EndOfStream) planets.Add(new(sr.ReadLine()));
+
+Console.WriteLine("1. feladat:");
+Console.WriteLine($"\t{planets.Count} db bolygü van a naprendszerben\n");
+
+Console.WriteLine("2. feladat:");
+double atlagHold = 0;
+foreach (Planet planet in planets)
+{
+    atlagHold += planet.Holdak;
+}
+atlagHold /= planets.Count;
+Console.WriteLine($"\tA naprendszer egy bolgyójának átlegosan {atlagHold} db holdja van\n");
