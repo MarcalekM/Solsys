1ac8ee65682281eb9bc2df310a5ab2a2fed6dd43
Máté Marcalek <matemarcalek@gmail.com>
2023-10-24 11:50:08 +0200

    3. feladat

--- Solsys/Planet.cs
+++ Solsys/Planet.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,14 +11,15 @@ namespace Solsys
     {
         public string Nev { get; set; }
         public int Holdak { get; set; }
-        public double   Terfogat { get; set; }
+        public double Terfogat { get; set; }
 
         public  Planet(string s)
         {
             var v = s.Split(';');
             Nev = v[0];
             Holdak = int.Parse(v[1]);
-            Terfogat = double.Parse(v[2]);
+            if (v[2].Contains('.')) Terfogat = double.Parse(v[2].Replace('.', ','));
+            else Terfogat = double.Parse(v[2]);
         }
     }
 }
--- Solsys/Program.cs
+++ Solsys/Program.cs
@@ -2,7 +2,7 @@
 
 List<Planet> planets = new();
 using StreamReader sr = new(
-    path: @"..\..\..\src.solsys.txt",
+    path: @"..\..\..\src\solsys.txt",
     encoding: System.Text.Encoding.UTF8);
 while (!sr.EndOfStream) planets.Add(new(sr.ReadLine()));
 
@@ -10,10 +10,10 @@ Console.WriteLine("1. feladat:");
 Console.WriteLine($"\t{planets.Count} db bolygü van a naprendszerben\n");
 
 Console.WriteLine("2. feladat:");
-double atlagHold = 0;
-foreach (Planet planet in planets)
-{
-    atlagHold += planet.Holdak;
-}
-atlagHold /= planets.Count;
-Console.WriteLine($"\tA naprendszer egy bolgyójának átlegosan {atlagHold} db holdja van\n");
+var f2 = planets.Average(x => x.Holdak);
+Console.WriteLine($"\tA naprendszer egy bolgyójának átlegosan {f2} db holdja van\n");
+
+Console.WriteLine("3. feladat:");
+var f3 = planets.OrderByDescending(p => p.Terfogat)
+                         .First();                        
+Console.WriteLine($"\tA legnagyobb bolygó a {f3.Nev}");
